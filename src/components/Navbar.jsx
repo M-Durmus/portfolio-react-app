7301f1f1cdc2fa4f2e0db1315230a05ef2daaa4b
M-Durmus <mdurmus9114@gmail.comm>
2022-04-10 00:23:02 +0300

    update reademefile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,8 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
+
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -99,7 +101,7 @@ const Navbar = () => {
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a
+            <a 
               className='flex justify-between items-center w-full text-gray-300'
               href='/'
             >
